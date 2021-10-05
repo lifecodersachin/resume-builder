8486029405bac682b868250f36faaf2b92085b20
Sachin Sajan <sachisajan.p98@gmail.com>
2021-10-05 22:10:30 +0530

    Initialized React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Resume from './components/resume';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Resume />
+    </div>
+  );
+}
+
+export default App;
